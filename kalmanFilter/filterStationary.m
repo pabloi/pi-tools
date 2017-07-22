a87465bd28583c62f7278c21ef02712ab8640f16
pabloi <itu.pablo@gmail.com>
2017-07-22 10:22:18 -0400

    Added basic Kalman Filter functions

---
+++ kalmanFilter/filterStationary.m
@@ -0,0 +1,40 @@
+function [X,P]=filterStationary(Y,A,C,Q,R,x0,P0,b,d)
+%filterStationary implements a Kalman filter assuming
+%stationary (fixed) noise matrices and system dynamics
+%The model is: x[k+1]=A*x[k]+b+v[k], v~N(0,Q)
+%y[k]=C*x[k]+d+w[k], w~N(0,R)
+
+%Init missing params:
+if nargin<6 || isempty(x0)
+  x0=zeros(size(A,1),1); %Column vector
+end
+if nargin<7 || isempty(P0)
+  P0=1e8 * eye(size(A));
+end
+if nargin<7 || isempty(b)
+  b=0;
+end
+if nargin<8 || isempty(d)
+  d=0;
+end
+
+%Size checks:
+%TODO
+
+%Do the filtering
+Xp=nan(size(A,1),size(Y,2));
+X=nan(size(A,1),size(Y,2));
+Pp=nan(size(A,1),size(A,1),size(Y,2));
+P=nan(size(A,1),size(A,1),size(Y,2));
+prevX=x0;
+prevP=P0;
+for i=1:size(Y,2)
+  [prevX,prevP]=predict(A,Q,prevX,prevP,b);
+  Xp(:,i)=prevX;
+  Pp(:,:,i)=prevP;
+  [prevX,prevP]=update(C,R,prevX,prevP,d);
+  X(:,i)=prevX;
+  P(:,:,i)=prevP;
+end
+
+end
