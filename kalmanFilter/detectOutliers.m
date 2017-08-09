13d3835ad153568b6ba710a9a4e0dd54d048cc07
Unknown <itu.pablo@gmail.com>
2017-08-08 20:44:29 -0400

    Changes to detectOuttliers
    Previous version did not scale. I am not sure current version works well either.

--- kalmanFilter/detectOutliers.m
+++ kalmanFilter/detectOutliers.m
@@ -1,19 +1,24 @@
 function [outlierIndx]=detectOutliers(y,x,P,C,R)
-m=length(y);
-th=20; %Threshold
+m=size(y,1);
+th=25; %Threshold
 expY=C*x;
 Py=C*P*C' + R;
 innov=y-expY;
 %logp=innov'*pinv(Py)*innov; %-1/2 of log(p) of observation
-auxTh=th-m*log(2*pi*det(Py));
+auxTh=th*m;%-log(det(2*pi*Py));
 auxLogP=pinv(Py)*innov;
 %innov.*auxLogP
-outlierIndx=innov.*auxLogP> auxTh; %Values of 1 indicate likely outliers
+auxDist=innov.*auxLogP;
+outlierSamples=sum(auxDist)>auxTh;
+outlierIndx=false(size(auxDist));
+outlierIndx(:,outlierSamples)=true;
+outlierIndx=auxDist> auxTh/m; %Values of 1 indicate likely outliers
 
-if any(outlierIndx)
-    disp(['Found outliers:' num2str(find(outlierIndx)')])
-    y
-end
+%if any(outlierIndx)
+%    disp(['Found outliers:' num2str(find(outlierIndx)')])
+%    y
+%end
+
 %ALT: do it recursively by finding the lowest log(p), if it passes the threshold
 %exclude it, then re-compute log(p) without considering it, find the second lowest
 %value, and so forth.
